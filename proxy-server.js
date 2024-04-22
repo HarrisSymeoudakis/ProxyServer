f46c2d4a19a2170158229a3d90dd8911d3aecae5
Harris Symeoudakis <harris.symeoudakis@gmail.com>
2024-04-22 15:03:43 +0300

    Update proxy-server.js

--- proxy-server.js
+++ proxy-server.js
@@ -5,7 +5,10 @@ const app = express();
 app.use(cors());
 
 const target = 'https://retail-services.cegid.cloud';
-
+app.use((req, res, next) => { 
+    console.log('Incoming request: ${req.method} ${req.url}'); 
+    next(); 
+});
 const proxyMiddleware = createProxyMiddleware({
     target,
     changeOrigin: true,
